1c13349b861771280122eef8705c513946c17c33
onik <155015348+sndgfn@users.noreply.github.com>
2025-11-18 16:09:24 +0600

    Add files via upload

---
+++ src/Pages/Test02/Test02.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const Test02 = () => {
+
+ 
+
+    return (
+       <>
+       <h2 className="text-xl">this is for test</h2>
+       
+       </>
+    );
+};
+
+export default Test02;
